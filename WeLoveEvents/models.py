8865d4ae2136886bfa883b8b417c70e6e353c058
Paola <vivenzi.paola03@gmail.com>
2025-05-08 23:15:33 +0200

    Migliorato database

--- WeLoveEvents/models.py
+++ WeLoveEvents/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Organization(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -10,6 +30,10 @@ class Event(models.Model):
     location = models.CharField(max_length=200)
     description = models.TextField()
     poster = models.ImageField()
+    organization = models.ManyToManyField(Organization, on_delete=models.CASCADE, related_name='events')
+    favourite = models.ManyToManyField(User, blank=True, related_name='favourite_events')
 
     def __str__(self):
         return self.name
+
+
