6dfd2aad490f33cf790398d5e5df8cd230020bb8
Paola <vivenzi.paola03@gmail.com>
2025-05-11 15:01:59 +0200

    Sistemato errori del database

--- WeLoveEvents/models.py
+++ WeLoveEvents/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
     email = models.EmailField()
     password = models.CharField(max_length=200)
     is_admin = models.BooleanField(default=False)
+    favourite = models.ManyToManyField('Event', related_name='favourite_events', blank=True, null=True)
+
+
 
     def __str__(self):
-        return self.name
+        return self.email
 
 class Organization(models.Model):
     id = models.AutoField(primary_key=True)
@@ -15,7 +19,9 @@ class Organization(models.Model):
     description = models.TextField()
     email = models.EmailField()
     password = models.CharField(max_length=200)
-    address = models.CharField(max_length=200, blank=True, null=True)
+    address = models.CharField(max_length=200)
+    logo = models.ImageField(upload_to='', default='default_logo.png')
+
 
     def __str__(self):
         return self.name
@@ -31,8 +37,7 @@ class Event(models.Model):
     description = models.TextField()
     poster = models.ImageField()
     organization = models.ManyToManyField(Organization, related_name='events')
-    favourite = models.ManyToManyField(User, related_name='favourite_events')
-
+ 
     def __str__(self):
         return self.name
 
